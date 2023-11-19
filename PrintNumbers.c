b0c5419fde6867a7c2bdae8c100737c7ab354db8
mayurisamanta <samantamayuri@gmail.com>
2023-11-19 21:26:01 +0530

    adding program to print certain range of numbers

---
+++ PrintNumbers.c
@@ -0,0 +1,13 @@
+/*Program to print certain range of Numbers*/
+
+#include <stdio.h>
+
+int main () {
+    int start = 1, end = 10;
+
+    for (int i = start; i <= end; i++) {
+        printf("i is %d \n", i);
+    }
+
+    return 0;
+}
